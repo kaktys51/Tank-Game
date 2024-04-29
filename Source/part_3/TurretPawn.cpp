66699fe1d2731d8ee838fb92f76ee81b4b0421e0
Denys Kruhlenkov <denis.burger51@gmail.com>
2024-04-29 18:25:50 +0300

    task_1 cpp updated

--- Source/part_3/TurretPawn.cpp
+++ Source/part_3/TurretPawn.cpp
@@ -5,6 +5,17 @@ ATurretPawn::ATurretPawn()
 {
 	PrimaryActorTick.bCanEverTick = true;
 
+	CapsuleComponent = CreateDefaultSubobject<UCapsuleComponent>(TEXT("CapsuleComponent"));
+	RootComponent = CapsuleComponent;
+
+	BaseMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("BaseMesh"));
+	BaseMesh->SetupAttachment(CapsuleComponent);
+
+	TurretMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("TurretMesh"));
+	TurretMesh->SetupAttachment(CapsuleComponent);
+
+	ProjectileSpawnPoint = CreateDefaultSubobject<USceneComponent>(TEXT("ProjectileSpawnPoint"));
+	ProjectileSpawnPoint->SetupAttachment(CapsuleComponent);
 }
 
 void ATurretPawn::BeginPlay()
