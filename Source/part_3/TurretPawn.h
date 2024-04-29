0c44332424f86a6f18072b97b86c39f7446db6d0
Denys Kruhlenkov <denis.burger51@gmail.com>
2024-04-29 18:12:04 +0300

    task_1 files added

--- Source/part_3/TurretPawn.h
+++ Source/part_3/TurretPawn.h
@@ -1,10 +1,11 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/Pawn.h"
+#include "Components/CapsuleComponent.h"
+#include "Components/StaticMeshComponent.h"
 #include "TurretPawn.generated.h"
+
 
 UCLASS()
 class PART_3_API ATurretPawn : public APawn
@@ -12,18 +13,27 @@ class PART_3_API ATurretPawn : public APawn
 	GENERATED_BODY()
 
 public:
-	// Sets default values for this pawn's properties
 	ATurretPawn();
 
 protected:
-	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
+public:
+
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = "Default")
+	TObjectPtr<UCapsuleComponent> CapsuleComponent;
+
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = "Default")
+	TObjectPtr<UStaticMeshComponent> BaseMesh;
+
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = "Default")
+	TObjectPtr<UStaticMeshComponent> TurretMesh;
+
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = "Default")
+	TObjectPtr<USceneComponent> ProjectileSpawnPoint;
+
 	virtual void Tick(float DeltaTime) override;
 
-	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
 };
