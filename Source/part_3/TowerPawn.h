2aeed9e04a2a0f67991e6fd1897e1dab48f1c971
Denys Kruhlenkov <denis.burger51@gmail.com>
2024-05-02 19:36:26 +0300

    task 2 new classes generated

---
+++ Source/part_3/TowerPawn.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "TurretPawn.h"
+#include "TowerPawn.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PART_3_API ATowerPawn : public ATurretPawn
+{
+	GENERATED_BODY()
+	
+};
