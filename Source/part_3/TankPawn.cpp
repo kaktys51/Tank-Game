f4141a9c1fda17834603e43c66ce4e20180a4f7c
Denys Kruhlenkov <denis.burger51@gmail.com>
2024-05-13 16:43:46 +0300

    Revert "c++ func added"
    This reverts commit 34f322eb748a402ef47764ce78e63f90d6f71782.

--- Source/part_3/TankPawn.cpp
+++ Source/part_3/TankPawn.cpp
@@ -1,24 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
 
 
 #include "TankPawn.h"
 
-ATankPawn::ATankPawn()
-{
-	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
-	SpringArm->SetupAttachment(CapsuleComponent);
-
-	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
-	Camera->SetupAttachment(SpringArm);
-}
-
-void ATankPawn::Move(float Amount)
-{
-}
-
-void ATankPawn::Turn(float Amount)
-{
-}
-
-void ATankPawn::Fire()
-{
-}
--- Source/part_3/TankPawn.h
+++ Source/part_3/TankPawn.h
@@ -4,36 +4,14 @@
 
 #include "CoreMinimal.h"
 #include "TurretPawn.h"
-#include "GameFramework/SpringArmComponent.h"
-#include "Camera/CameraComponent.h"
 #include "TankPawn.generated.h"
 
-
+/**
+ * 
+ */
 UCLASS()
 class PART_3_API ATankPawn : public ATurretPawn
 {
 	GENERATED_BODY()
-
-public:
-
-	ATankPawn();
-
-protected:
-
-public:
-
-	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = "Default")
-	TObjectPtr<USpringArmComponent> SpringArm;
-
-	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = "Default")
-	TObjectPtr<UCameraComponent> Camera;
-
-	UFUNCTION(BlueprintCallable, Category = "Tank Actions")
-	void Move(float Amount);
-
-	UFUNCTION(BlueprintCallable, Category = "Tank Actions")
-	void Turn(float Amount);
-
-	UFUNCTION(BlueprintCallable, Category = "Tank Actions")
-	void Fire();
+	
 };
