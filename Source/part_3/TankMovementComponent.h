563012fa515da8517769c77ccdd8d003c8620b07
Denys Kruhlenkov <denis.burger51@gmail.com>
2025-01-30 16:41:10 +0200

    movement component created

---
+++ Source/part_3/TankMovementComponent.h
@@ -0,0 +1,14 @@
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "TankMovementComponent.generated.h"
+
+
+UCLASS()
+class PART_3_API UTankMovementComponent : public UCharacterMovementComponent
+{
+	GENERATED_BODY()
+	
+};
